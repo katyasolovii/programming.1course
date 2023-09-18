de011aa197567e442d12c1f5d95d24eaf39f7b7e
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 15:58:48 +0300

    Update  mathematical_expression _2.py

--- lab1/ mathematical_expression _2.py
+++ lab1/ mathematical_expression _2.py
@@ -1,5 +1,5 @@
 x,y  = map(float, input().split())
 
-first_part = (x**2-2*x*y+4*y**2)/(x+5)
-second_part = (3*x**2 - y**2)/(y-7)
-print (f"{first_part + second_part:.3f}")
+first_part = (x * * 2 - 2 * x * y + 4 * y * * 2) / (x + 5)
+second_part = (3 * x * * 2 - y * * 2) / (y - 7)
+print(f"{first_part + second_part:.3f}")
