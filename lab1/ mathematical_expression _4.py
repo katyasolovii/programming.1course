a04cc8a99f72c319c79906fbc01f161a54c309ee
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 17:19:23 +0300

    Update  mathematical_expression _4.py

--- lab1/ mathematical_expression _4.py
+++ lab1/ mathematical_expression _4.py
@@ -4,6 +4,7 @@ x, y = map(float, input().split())
 
 first_part = (2 * x * y) / (math.sqrt(pow(x, 2)+ pow(y, 2)))
 second_part = pow(x+y-1, 2) / (x * y)
+
 print(f"{first_part - second_part:.3f}")
 
 
