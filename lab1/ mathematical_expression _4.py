ad6b4dfd4c90d941bf019077053973438b7f3acf
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 17:16:13 +0300

    Update  mathematical_expression _4.py

--- lab1/ mathematical_expression _4.py
+++ lab1/ mathematical_expression _4.py
@@ -4,5 +4,6 @@ first_part = (2 * x * y) / (math.sqrt(pow(x, 2)+ pow(y, 2)))
 second_part = pow(x+y-1, 2) / (x * y)
 print(f"{first_part - second_part:.3f}")
 
+
 """
 Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14209020
