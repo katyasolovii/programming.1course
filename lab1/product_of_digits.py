30c07cf513e0577e36006ddcc8005d25093fedd3
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 17:16:46 +0300

    Update product_of_digits.py

--- lab1/product_of_digits.py
+++ lab1/product_of_digits.py
@@ -1,6 +1,3 @@
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14202874
-"""
-
 x = abs(int(input()))
 
 first_dig = x % 10
@@ -8,3 +5,7 @@ second_dig = x // 10 % 10
 third_dig = x // 100 
 
 print(first_dig * second_dig * third_dig) 
+
+
+"""
+Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14202874
