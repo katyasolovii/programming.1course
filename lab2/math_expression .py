7cf58ea8700f700ba4b6c0d64a4b27d8fd7632bf
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 16:01:37 +0300

    Update math_expression .py

--- lab2/math_expression .py
+++ lab2/math_expression .py
@@ -1,7 +1,7 @@
 x = int(input())
 
 if x >= 0:
- print(pow(x,3) + 2*pow(x,2) + 4*x - 6)
+ print(pow(x,3) + 2 * pow(x,2) + 4 * x - 6)
 else:
     if x < 0:
-       print(pow(x, 3) - 7*x)
+       print(pow(x, 3) - 7 * x)
