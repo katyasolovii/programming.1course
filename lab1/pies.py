d95af2488a648a34f04a1d80e3084a5722c8a661
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 16:00:00 +0300

    Update pies.py

--- lab1/pies.py
+++ lab1/pies.py
@@ -1,4 +1,4 @@
 a,b,n = map(int, input().split())
 
-price = (a + (b/100)) * n
-print (int(price), int(round(price%1*100, 2)))
+price = (a + (b / 100)) * n
+print(int(price), int(round(price % 1 * 100, 2)))
