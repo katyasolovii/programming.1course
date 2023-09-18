ea675a3115653a664ef045706e896b380786b7a4
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 15:59:37 +0300

    Update formula_geron.py

--- lab1/formula_geron.py
+++ lab1/formula_geron.py
@@ -5,7 +5,7 @@ a,b,c,d,f = [float (x) for x in input(). split()]
 p1 = (a + b + f) / 2
 p2 = (f + d + c) / 2
 
-s1 = sqrt (p1 * (p1 - a) * (p1 - b) * (p1 - f)) 
-s2 = sqrt (p2 * (p2 - f) * (p2 - d) * (p2 - c)) 
+s1 = sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - f)) 
+s2 = sqrt(p2 * (p2 - f) * (p2 - d) * (p2 - c)) 
 
 print(s1 + s2)
