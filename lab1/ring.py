064d586345c3eb9c4827f50013252667c8b5126f
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 17:15:33 +0300

    Update ring.py

--- lab1/ring.py
+++ lab1/ring.py
@@ -1,8 +1,7 @@
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14210641
-"""
-
-
 import math
 s, R = map(float, input().split())
 r = math.sqrt(pow(R, 2) - (s / math.pi))
 print(f"{r:.2f}")
+
+"""
+Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14210641
