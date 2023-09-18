343af5393c8f8ca83c9d60639bb57de946d502c3
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 15:49:17 +0300

    Update product_of_digits.py

--- lab1/product_of_digits.py
+++ lab1/product_of_digits.py
@@ -1,7 +1,7 @@
-x = abs (int(input()))
+x = abs(int(input()))
 
-first_dig = x%10
-second_dig = x//10%10
-third_dig = x//100 
+first_dig = x % 10
+second_dig = x // 10 % 10
+third_dig = x // 100 
 
 print(first_dig * second_dig * third_dig) 
