1cc035682d246838ab74b3e6cf04bce051658526
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-18 15:53:31 +0300

    Update grades.py

--- lab2/grades.py
+++ lab2/grades.py
@@ -1,13 +1,13 @@
 x = int(input())
 
-if 1<= x <= 3:
+if 1 <= x <= 3:
     print("Initial")
 else:
-    if 4<= x <= 6:
+    if 4 <= x <= 6:
         print("Average")
     else:
-        if 7<= x <= 9:
+        if 7 <= x <= 9:
             print("Sufficient")
         else:
-            if 10<= x <= 12:
+            if 10 <= x <= 12:
                 print("High")
