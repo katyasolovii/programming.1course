446265c52c0e4f1d56b3ebda968459033509d274
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-09-30 17:35:19 +0300

    Delete factorial_2.py
    created a new folder lab3 and added it to the branch through vsc

--- factorial_2.py
+++
@@ -1,9 +0,0 @@
-"""
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14378723
-"""
-
-n = int(input())
-factorial = 1
-for i in range(1, n + 1):
-    factorial *= i
-print(len(str(factorial)))
