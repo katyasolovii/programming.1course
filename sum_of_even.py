7f69fb567a17a1619d0644ca547e546031b346a5
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-10-05 23:18:46 +0300

    Update sum_of_even.py
    redid without lists

--- sum_of_even.py
+++ sum_of_even.py
@@ -1,14 +1,13 @@
 """
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14530963
+Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14571390
 """
 
 n = int(input())
 digits = [n]
 summ = 0 
 while n != 0:
-    n = int(input())
-    digits.append(n)
-for n in digits:
     if n % 2 == 0:
         summ += n
+    n = int(input())
+    digits.append(n)
 print(summ)
