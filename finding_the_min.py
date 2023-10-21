347a72d7436b6a21a4972ceef209115c6b474fe8
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-10-21 23:59:10 +0300

    Delete finding_the_min.py

--- finding_the_min.py
+++
@@ -1,9 +0,0 @@
-"""
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14814591
-"""
-
-n = int(input())
-lst = [int(el) for el in input().split()]
-for el in range(len(lst)):
-    minimum = min(lst)
-print(minimum)
