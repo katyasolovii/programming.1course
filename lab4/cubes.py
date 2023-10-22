8771e043d5be1ea2b6d7ff965190a630a9ef6210
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-10-22 23:23:24 +0300

    Delete lab4 directory

--- lab4/cubes.py
+++
@@ -1,9 +0,0 @@
-"""
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14453828
-"""
-import math
-n = int(input())
-i = 1
-while pow(i, 3) < n:
-    print(pow(i, 3), end = ' ')
-    i += 1
