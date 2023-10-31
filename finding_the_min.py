a39c8e52754599c68a1721e8b4f0f14edcfacea1
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-11-01 00:06:32 +0200

    Update finding_the_min.py
    improved

--- finding_the_min.py
+++ finding_the_min.py
@@ -1,9 +1,7 @@
 """
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14814591
+Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14953966
 """
 
 n = int(input())
 lst = [int(el) for el in input().split()]
-for el in range(len(lst)):
-    minimum = min(lst)
-print(minimum)
+print(min(lst))
