49af01c837e6eb26eeb70bbdbfe1146f9e5c4322
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-11-13 00:07:58 +0200

    Update lcm.py
    changed

--- lcm.py
+++ lcm.py
@@ -1,14 +1,22 @@
 """
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/14996593
+Посилання на розв'язок: https://www.eolymp.com/uk/submissions/15098203
 """
-#gcd - НСД
-#lcm - НСК; НСК(a;b) = a * b // НСД(a,b)
-import math
+# gcd - НСД
+# lcm - НСК; НСК(a;b) = a * b // НСД(a,b)
+
+
+def find_gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
 
 def lcm(n):
     a = 1
     for b in range(1, n + 1):
-        a = a * b // math.gcd(a, b)
+        a = a * b // find_gcd(a, b)
     return a
+
+
 n = int(input())
 print(lcm(n))
