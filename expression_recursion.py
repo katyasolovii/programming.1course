40eafe6676dff8ffd6ac79fe42aca2bdfd798543
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-11-18 16:58:24 +0200

    Update expression_recursion.py
    changed

--- expression_recursion.py
+++ expression_recursion.py
@@ -1,6 +1,6 @@
 def expressions_rec(n):
 # перевірка на дужки
-    open_p = n.find("(") # перший пріорітет - дужки
+    open_p = n.find("(") # перший пріоритет - дужки
     if open_p != -1: # розташування "("
         close_p = n.find(")")
         if close_p != -1: # розташування ")"
@@ -8,7 +8,7 @@ def expressions_rec(n):
             n = n[:open_p] + str(inside_n) + n[close_p + 1:] # видаляємо все що було в дужках, замість цього вставляємо результат обчисленого виразу
             return expressions_rec(n)
     tokens = n.split() # розділяємо вираз ['8.2', '-', '12', '/', '3'], ['8.2', '-', '4.0'], ['2', '+', '4.2', '*', '4.199999999999999'], ['2', '+', '17.639999999999997']
-    for num, token in enumerate(tokens): # другий праорітет на дію * та /
+    for num, token in enumerate(tokens): # другий пріоритет на дію * та /
         if token == "*":
             res = float(tokens[num - 1]) * float(tokens[num + 1])
             tokens = tokens[:num - 1] + [str(res)] + tokens[num + 2:] # замінюємо в tokens дію множення на результат цієї дії 
@@ -17,7 +17,7 @@ def expressions_rec(n):
             res = float(tokens[num - 1]) / float(tokens[num + 1])
             tokens = tokens[:num - 1] + [str(res)] + tokens[num + 2:] # замінюємо в tokens дію ділення на результат цієї дії 
             return expressions_rec(' '.join(tokens)) # новим вираз йде на початок рекурсії
-    res = float(tokens[0]) # третій пріорітет на дію + та -
+    res = float(tokens[0]) # третій пріоритет на дію + та -
     for num in range(1, len(tokens)): 
         if tokens[num] == "+":
             res += float(tokens[num + 1])
