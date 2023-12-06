fa12aad7b13a67a8a1d2aeec9e00217f537a002b
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2023-12-07 01:12:25 +0200

    Update 13_7_a.py

--- 13_7_a.py
+++ 13_7_a.py
@@ -4,4 +4,4 @@
 with open("/Users/katyasolovii/Desktop/programming/lab10/words.txt", "rt") as my_file:
     content = my_file.read()
     words = content.split()
-print(max(words, key = len)) # максимальне слово за довжиною 
+print(max(words, key=len)) # максимальне слово за довжиною 
