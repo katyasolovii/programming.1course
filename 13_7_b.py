f7f05efc8df8863ffaca7151a98f395d2499634b
katyasolovii <soloviyk@gmail.com>
2024-01-24 18:18:22 +0200

    sorted the files into folders

--- 13_7_b.py
+++
@@ -1,7 +0,0 @@
-# звизначення кількості слів у файлі
-
-
-with open("/Users/katyasolovii/Desktop/programming/lab10/words.txt", "rt") as my_file:
-    content = my_file.read()
-    words = content.split()
-print(len(words))
