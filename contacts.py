c255a3ebfada7050d0ae4fd2770ba6bb2fcfe623
katyasolovii <soloviyk@gmail.com>
2024-01-24 19:23:56 +0200

    deleted the files

--- contacts.py
+++
@@ -1,8 +0,0 @@
-"""
-Посилання на розв'язок: https://www.eolymp.com/uk/submissions/15038070
-"""
-
-n = int(input())
-phone_numbers = [int(el) for el in input().split()]
-contacts = set(phone_numbers)
-print(len(contacts))
