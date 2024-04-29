804e997d6eb9bbcff8e0e31d5980b064f7fd33e9
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2024-04-29 19:19:34 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -10,18 +10,18 @@ max_abs_num = None
 summ_num = 0
 for num in numbers:
     # для порівняння дробів, зведемо їх до спільного дільника і порівняємо їхні чисельники
-    if max_num is None or (num.denominator > 0 and num.numerator > 0 and num.numerator * max_num.denominator > max_num.numerator * num.denominator):
+    if max_num is None or (num.denominator > 0 and num.numerator > 0 and num > max_num):
         max_num = num
-    abs_num = Rational_numbers.__abs__(num)
+    abs_num = abs(num)
     if max_abs_num is None:
         max_abs_num = num
     else:
-        abs_ls_max = Rational_numbers.__abs__(max_abs_num)
-        if abs_num.numerator * abs_ls_max.denominator > abs_ls_max.numerator * abs_num.denominator:
+        abs_ls_max = abs(max_abs_num)
+        if abs_num > abs_ls_max:
             max_abs_num = num
-    summ_num = Rational_numbers.__add__(summ_num, num)
+    summ_num += num
 
 print("Максимальне число:", max_num)
 print("Максимальне число за модулем:", max_abs_num)
-arithmetic = Rational_numbers.__str__(Rational_numbers.__truediv__(summ_num, Rational_numbers(len(numbers), 1)))
+arithmetic = float(summ_num / len(numbers))
 print("Середнє арифметичне значення:", arithmetic)
