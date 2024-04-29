902c765bace003b95cae4db5931a2bd8dd615f73
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2024-04-29 19:19:16 +0300

    Update rational_num.py

--- rational_num.py
+++ rational_num.py
@@ -8,12 +8,11 @@ class Rational_numbers:
         """
         Конструктор для створення нескоротного дробу за допомогою виклику приватного статичного методу _reduce().
         Дріб m/n, де n - numerator, d - denominator.
-        За умовою завдання n > 0.
+        За умовою завдання n > 0. НЕ ЗА УМОВОЮ
         """
         if d == 0:
             raise ValueError("Знаменник не може бути 0.")
         self._n, self._d = self._reduce(n, d)
-
 
     @staticmethod
     def _reduce(n, d):
@@ -21,18 +20,18 @@ class Rational_numbers:
             common_divisor = gcd(n, d)
             n //= common_divisor
             d //= common_divisor
+        if d < 0:
+            n = -n
+            d = -d
         return n, d
-    
 
     @property
     def numerator(self):
         return self._n
 
-
     @property
     def denominator(self):
         return self._d
-    
 
     @classmethod
     def from_string(cls, string):
@@ -47,66 +46,58 @@ class Rational_numbers:
         denominator = int(denominator)
         return cls(numerator, denominator)
 
-
     def __add__(self, other):
         if isinstance(other, int):
-            other = Rational_numbers(other)
+            other = Rational_numbers(other, 1)
         return Rational_numbers(self.numerator * other.denominator + other.numerator * self.denominator, self.denominator * other.denominator)
 
-
     def __radd__(self, other):
-        return other.__add__(self)
-
+        return self.__add__(other)
 
     def __sub__(self, other):
         if isinstance(other, int):
             other = Rational_numbers(other)
         return Rational_numbers(self.numerator * other.denominator - other.numerator * self.denominator, self.denominator * other.denominator)
 
-
     def __rsub__(self, other):
         return other.__sub__(self) 
-
 
     def __mul__(self, other):
         if isinstance(other, int):
             other = Rational_numbers(other)
         return Rational_numbers(self.numerator * other.numerator, self.denominator * other.denominator)
 
-
     def __rmul__(self, other):
         return other.__mul__(self)
 
-
     def __truediv__(self, other):
         if isinstance(other, int):
-            other = Rational_numbers(other)
+            other = Rational_numbers(other, 1)
         if other.numerator == 0:
             raise ValueError("Ділення на 0.")
         return Rational_numbers(self.numerator * other.denominator, self.denominator * other.numerator)
     
-
     def __rtruediv__(self, other):
         if self.numerator == 0:
             raise ValueError("Ділення на 0.")
-        return other.__truediv__(self)
-    
+        return self.__truediv__(other)
 
     def __abs__(self):
         return Rational_numbers(abs(self.numerator), abs(self.denominator))
-    
 
     def sign(self):
         """
         Повертає знак дробу:
-        якщо додатній дріб == 1,
-        якщо від'ємний дріб == -1.
+        якщо чисельник додатній == 1,
+        якщо чисельник від'ємний == -1,
+        якщо чисельник 0 == 0.
         """
-        if self.numerator > 0 and self.denominator > 0:
+        if self.numerator > 0:
             return 1
-        if self.numerator < 0 or self.denominator < 0:
+        elif self.numerator < 0:
             return -1
-
+        else:
+            return 0 
 
     def integer(self):
         """
@@ -117,12 +108,10 @@ class Rational_numbers:
         else:
             return False
 
-
     def __eq__(self, other):
         if isinstance(other, int):
             other = Rational_numbers(other)
         return self.numerator == other.numerator and self.denominator == other.denominator
-
 
     def inverse(self):
         """
@@ -132,12 +121,25 @@ class Rational_numbers:
             raise ValueError("Знаменник оберненого дробу 0.")
         return Rational_numbers(self.denominator, self.numerator)
 
-
     def __str__(self):
         if self.denominator == 1:
             return str(self.numerator)
-        return str(self.numerator / self.denominator)
+        return f"{self.numerator}/{self.denominator}"
 
+    def __float__(self):
+        return self.numerator / self.denominator
 
     def __repr__(self):
         return f"Rational_numbers(numerator = {self.numerator}, denominator = {self.denominator})"
+    
+    def __lt__(self, other):
+        return (self.numerator * other.denominator) < (other.numerator * self.denominator)
+    
+    def __le__(self, other):
+        return (self.numerator * other.denominator) <= (other.numerator * self.denominator)
+
+    def __gt__(self, other):
+        return (self.numerator * other.denominator) > (other.numerator * self.denominator)
+
+    def __ge__(self, other):
+        return (self.numerator * other.denominator) >= (other.numerator * self.denominator)
