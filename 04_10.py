33e5142838546f87e18d29a4dd64e17423d33d19
katyasolovii <144212333+katyasolovii@users.noreply.github.com>
2024-06-25 22:38:14 +0300

    Update 04_10.py

--- 04_10.py
+++ 04_10.py
@@ -1,6 +1,9 @@
 n = int(input())
 d = 1
-for i in range(1, n + 1):
-    if i % 8 == 0:
-        d *= i
-print(d)
+if n >= 8:
+    for i in range(1, n + 1):
+        if i % 8 == 0:
+            d *= i
+    print(d)
+else:
+    print(0)
